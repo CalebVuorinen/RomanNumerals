5119791bd74cf09f58bebd835889c5dc3a8ea939
Kalle Vuorinen <kalle.vuorinen@suprakraft.com>
2018-10-12 21:57:21 +0300

    Roman numeral converter, sunny cases

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,68 @@
+/*
+	1 I 10 X 100 C
+	2 II 20 XX 200 CC
+	3 III 30 XXX 300 CCC
+	4 IV 40 XL 400 CD
+	5 V 50 L 500 D
+	6 VI 60 LX 600 DC
+	7 VII 70 LXX 700 DCC
+	8 VIII 80 LXXX 800 DCCC
+	9 IX 90 XC 900 CM
+ */
+/*
+	1984	=	MCMLXXXIV
+	2014	=	MMXIV
+ */
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+
+    private static int convertLetterToNumber(char roman) {
+        switch (roman) {
+           case 'I':  return 1;
+           case 'V':  return 5;
+           case 'X':  return 10;
+           case 'L':  return 50;
+           case 'C':  return 100;
+           case 'D':  return 500;
+           case 'M':  return 1000;
+           default:   return 0;
+        }
+     }
+    
+	public static int convertToInteger(String romanNum) {
+		 if (romanNum.length() == 0) {
+			 return 0;
+		 }
+		 // Convert to upper case letters for transfer
+		 romanNum = romanNum.toUpperCase(); 
+          
+		int arabicNumber = 0;
+		int i = 0;
+		while (i < romanNum.length()) {
+			// Pick letters one by one
+			char romanLetter = romanNum.charAt(i);
+			int convertedNumber = convertLetterToNumber(romanLetter);
+			i++;
+			
+			// Case of last letter
+            if (i == romanNum.length()) {
+             arabicNumber += convertedNumber;
+          } else {
+        	  int nextConvertedNumber = convertLetterToNumber(romanNum.charAt(i));
+        	  
+              if (nextConvertedNumber > convertedNumber) {
+                  // Combine the 2 letters (for example IV = 4, meaning that 5-1 = 4)
+            	  arabicNumber += (nextConvertedNumber - convertedNumber);
+            	  i++;
+             } else {
+            	 // Just add the number (for example VI = 6, 5 + 1 = 6)
+            	 arabicNumber += convertedNumber;
+             }
+              
+          }
+		}
+
+		return arabicNumber;
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,129 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+/*
+1 I 10 X 100 C
+2 II 20 XX 200 CC
+3 III 30 XXX 300 CCC
+4 IV 40 XL 400 CD
+5 V 50 L 500 D
+6 VI 60 LX 600 DC
+7 VII 70 LXX 700 DCC
+8 VIII 80 LXXX 800 DCCC
+9 IX 90 XC 900 CM
+ */
+
+/*
+1984	=	MCMLXXXIV
+2014	=	MMXIV
+*/
 
 public class TestRomanNumerals {
 
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_roman_convertSingleLetters() {
+		// Arrange
+		int returnedArabicI; 
+		int returnedArabicV;
+		int returnedArabicX;
+		int returnedArabicL;
+		int returnedArabicC;
+		int returnedArabicD; 
+		
+		// Act
+		returnedArabicI = RomanNumerals.convertToInteger("I");
+		returnedArabicV = RomanNumerals.convertToInteger("V");
+		returnedArabicX = RomanNumerals.convertToInteger("X");
+		returnedArabicL = RomanNumerals.convertToInteger("L");
+		returnedArabicC = RomanNumerals.convertToInteger("C");
+		returnedArabicD = RomanNumerals.convertToInteger("D");
+		
+		// Assert
+		assertEquals(1, returnedArabicI);
+		assertEquals(5, returnedArabicV);
+		assertEquals(10, returnedArabicX);
+		assertEquals(50, returnedArabicL);
+		assertEquals(100, returnedArabicC);
+		assertEquals(500, returnedArabicD);
+	}
+	
+	@Test
+	public void test_roman_convertDoubleLetters() {
+		// Arrange
+		int returnedArabicII; 
+		int returnedArabicIV;
+		int returnedArabicVI;
+		int returnedArabicIX;
+		int returnedArabicXI;
+		int returnedArabicXV;
+		int returnedArabicXX;
+		int returnedArabicXL;
+		int returnedArabicLX;
+		int returnedArabicXC;
+		int returnedArabicCX;
+		int returnedArabicCC;
+		int returnedArabicCD;
+		int returnedArabicDC;
+		int returnedArabicCM;
+		int returnedArabicMC;
+		
+		// Act
+		returnedArabicII = RomanNumerals.convertToInteger("II");
+		returnedArabicIV = RomanNumerals.convertToInteger("IV");
+		returnedArabicVI = RomanNumerals.convertToInteger("VI");
+		returnedArabicIX = RomanNumerals.convertToInteger("IX");
+		returnedArabicXI = RomanNumerals.convertToInteger("XI");
+		returnedArabicXV = RomanNumerals.convertToInteger("XV");
+		returnedArabicXX = RomanNumerals.convertToInteger("XX");
+		returnedArabicXL = RomanNumerals.convertToInteger("XL");
+		returnedArabicLX = RomanNumerals.convertToInteger("LX");
+		returnedArabicXC = RomanNumerals.convertToInteger("XC");
+		returnedArabicCX = RomanNumerals.convertToInteger("CX");
+		returnedArabicCC = RomanNumerals.convertToInteger("CC");
+		returnedArabicCD = RomanNumerals.convertToInteger("CD");
+		returnedArabicDC = RomanNumerals.convertToInteger("DC");
+		returnedArabicCM = RomanNumerals.convertToInteger("CM");
+		returnedArabicMC = RomanNumerals.convertToInteger("MC");
+		
+		// Assert
+		assertEquals(2, returnedArabicII);
+		assertEquals(4, returnedArabicIV);
+		assertEquals(6, returnedArabicVI);
+		assertEquals(9, returnedArabicIX);
+		assertEquals(11, returnedArabicXI);
+		assertEquals(15, returnedArabicXV);
+		assertEquals(20, returnedArabicXX);
+		assertEquals(40, returnedArabicXL);
+		assertEquals(60, returnedArabicLX);
+		assertEquals(90, returnedArabicXC);
+		assertEquals(110, returnedArabicCX);
+		assertEquals(200, returnedArabicCC);
+		assertEquals(400, returnedArabicCD);
+		assertEquals(600, returnedArabicDC);
+		assertEquals(900, returnedArabicCM);
+		assertEquals(1100, returnedArabicMC);
 	}
 
+	@Test
+	public void test_roman_convertComplexLetters() {
+		/*
+		1984	=	MCMLXXXIV
+		2014	=	MMXIV
+		 */		
+
+		// Arrange
+		int returnedArabicMCMLXXXIV; 
+		int returnedArabicMMXIV;
+		
+		// Act
+		returnedArabicMCMLXXXIV = RomanNumerals.convertToInteger("MCMLXXXIV");
+		returnedArabicMMXIV = RomanNumerals.convertToInteger("MMXIV");
+		
+		// Assert
+		assertEquals(1984, returnedArabicMCMLXXXIV);
+		assertEquals(2014, returnedArabicMMXIV);
+	}
+	
+	
 }
